7b69175d249803c1f5dce0cb14ba2dfdc46fa514
Nickinfinity <nikhilg.me@gmail.com>
2021-05-29 16:06:32 +0530

    add sorting fucntionality for luck and skill rank

--- LuckFactor.cpp
+++ LuckFactor.cpp
@@ -22,17 +22,56 @@ using namespace std;
 class Person
 {
 public:
+    static int count;
     int luck;
     int skills_talent;
-    int luckPosition;
-    int skillsPosition;
+    int finalRank;
+    int luckRank;
+    int skillsRank;
     int index;
 
-    Person()
+    Person(int lower, int upper)
     {
+        this->count = count + 1; // keep track of index
+        this->luck = generateRandom(lower, upper);
+        this->skills_talent = generateRandom(lower, upper);
+        this->index = count;
+    }
+
+    bool compareLuck(Person *p1, Person *p2)
+    {
+
+        return (p1->luck < p2->luck);
+    }
+
+    bool compareSkills(Person *p1, Person *p2)
+    {
+        return (p1->skills_talent < p2->skills_talent);
+    }
+
+    static void sortOnLuck(vector<Person *> persons)
+    {
+        sort(persons.begin(), persons.end(), compareLuck);
+        for (int i = 0; i < persons.size(); i++)
+        {
+            persons[i]->luckRank = i + 1;
         }
+    }
+
+        static void sortOnSkills(vector<Person *> persons)
+    {
+        sort(persons.begin(), persons.end(), compareSkills);
+        for (int i = 0; i < persons.size(); i++)
+        {
+            persons[i]->skillsRank = i + 1;
+        }
+    }
 };
 
+//set static count
+int Person::count = 0; // set initial count to 0
+
+//generate random number for luckRank and skillRank
 int generateRandom(int lower, int upper) //[lower,upper]
 {
 
